dd720f835b5907172749554edfaef9054b2cf852
nunesemi <emilianunescabral@gmail.com>
2024-09-08 22:55:15 -0300

    feat: criação de serializers + urls e views

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.get_usuarios, name='get_all_usuarios'),
+    path("usuario/<str:id>", views.get_by_id)
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Usuario
+from .serializers import UsuarioSerializer
+
+import json
+
+@api_view(['GET'])
+def get_usuarios(request):
+    
+    if request.method == 'GET':
+        
+        usuarios = Usuario.objects.all()
+        
+        serializer = UsuarioSerializer(usuarios, many=True)
+        
+        return Response(serializer.data)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_by_id(request, id):
+        
+    if request.method == 'GET':
+        try:
+            usuario = Usuario.objects.get(pk="euzinha")
+            print(f"olha o usuário: {usuario}")
+            serializer = UsuarioSerializer(usuario)
+        except:
+            print(Exception)
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        
+        return Response(serializer.data)
+
+# @api_view(['GET', 'POST', 'PUT', 'DELETE'])
+# def usuario_manager(request):
+    
+#     if request.method == 'GET':
+        
+#         try:
+#             request.GET['nome']:
+                
+#                 nome = request.GET[nome]  
+                
+                 
